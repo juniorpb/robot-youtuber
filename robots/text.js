c0060a45531ffea2de0170c5829d9545ec38ca28
Junior Silva <juniorfoxld@gmail.com>
2019-04-14 16:25:27 -0300

    Implementando Inteligencia Artificial do watson no reconhecimento das premissas

--- robots/text.js
+++ robots/text.js
@@ -2,16 +2,28 @@ const algorithmia 				= require('algorithmia')
 const algorithmiaApiKey 		= require('../credentials/algorithmia.json').apiKey
 const sentenceBoundaryDetection	= require('sbd')
 
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
+ 
+const nlu = new NaturalLanguageUnderstandingV1({
+  iam_apikey: watsonApiKey,
+  version: '2018-04-05',
+  url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+})
+
+
 async function robot(content) {
 	await fetchContentFromWikipedia(content)
 	sanitizeContent(content)
 	breakContentIntoSentences(content)
+	limitMaximumSentences(content)
+	await fetchKeywordsOfAllSentences(content)
 
 	async function fetchContentFromWikipedia(content) {
 		const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
 		const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
 		const wikipediaResponde = await wikipediaAlgorithm.pipe({
-			"lang" : "pt",
+			"lang" : "pt", // set linguage
 			"articleName" : content.searchTerm
 		})
 		const wikipediaContent = wikipediaResponde.get()
@@ -59,6 +71,42 @@ async function robot(content) {
 			})
 		})
 	}
+
+	// total de sentences
+	function limitMaximumSentences(content) {
+		content.sentences = content.sentences.slice(0, content.maximumSentences)
+	}
+
+	// adc sentence na estrutura de dados
+	async function fetchKeywordsOfAllSentences(content) {
+		for (const sentence of content.sentences) {
+			sentence.keywords = await fetWatsonAndReturnKeywords(sentence.text)
+		}
+	}
+
+	// retorna palavras chaves de uma sentence
+	async function fetWatsonAndReturnKeywords(sentence) {
+		return new Promise((resolve, reject) =>{
+			nlu.analyze({
+				text: sentence,
+				features: {
+					keywords: {}
+				}
+			}, (error, response) => {
+				if (error) {
+					throw error
+				}
+
+				// retorna arraylist com palavras chaves
+				const keywords = response.keywords.map((keywords) => { 
+					return keywords.text
+				})
+
+				resolve(keywords)
+			})
+		})
+	}
+
 }
 
 
