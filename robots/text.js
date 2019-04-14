9466cf0fa95ec77794449f7ee140c0a5bfac244a
Junior Silva <juniorfoxld@gmail.com>
2019-04-14 14:45:22 -0300

    Tradução da busca na Wikipedia para pt-br

--- robots/text.js
+++ robots/text.js
@@ -10,7 +10,10 @@ async function robot(content) {
 	async function fetchContentFromWikipedia(content) {
 		const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
 		const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
-		const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm)
+		const wikipediaResponde = await wikipediaAlgorithm.pipe({
+			"lang" : "pt",
+			"articleName" : content.searchTerm
+		})
 		const wikipediaContent = wikipediaResponde.get()
 
 		// Salvando busca na estrutura de dados
