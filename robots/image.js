104d77d937385702b1392f21e99a4c00a94232eb
Junior Silva <juniorfoxld@gmail.com>
2019-04-17 00:45:28 -0300

    tratando imagens

--- robots/image.js
+++ robots/image.js
@@ -1,4 +1,6 @@
 const imageDownloader = require('image-downloader')
+const path = require('path')
+const gm = require('gm').subClass({imageMagick: true})
 const google = require('googleapis').google
 const customSearch = google.customsearch('v1')
 const state = require('./state.js')
@@ -9,7 +11,11 @@ async function robot() {
 	const content = state.load()
 
 	// await fetchImageOfAllSentences(content)
-	await downloadAllImagem(content)
+	// await downloadAllImagem(content)
+	//await convertAllImages(content)
+	//await createAllSentenceImages(content)
+	await createYouTubeThumbnail()
+
 	// state.save(content)
 
 	async function fetchImageOfAllSentences(content){
@@ -75,6 +81,126 @@ async function robot() {
 		return imageDownloader.image({
 			url: url, 
 			dest: `./content/${fileName}`
+		})
+	}
+
+	async function convertAllImages(content) {
+	    for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+	      await convertImage(sentenceIndex)
+	    }
+	  }
+
+	  async function convertImage(sentenceIndex) {
+	    return new Promise((resolve, reject) => {
+	      const inputFile = `./content/${sentenceIndex}-original.png[0]`
+	      const outputFile = `./content/${sentenceIndex}-converted.png`
+	      const width = 1920
+	      const height = 1080
+
+	      gm()
+	        .in(inputFile)
+	        .out('(')
+	          .out('-clone')
+	          .out('0')
+	          .out('-background', 'white')
+	          .out('-blur', '0x9')
+	          .out('-resize', `${width}x${height}^`)
+	        .out(')')
+	        .out('(')
+	          .out('-clone')
+	          .out('0')
+	          .out('-background', 'white')
+	          .out('-resize', `${width}x${height}`)
+	        .out(')')
+	        .out('-delete', '0')
+	        .out('-gravity', 'center')
+	        .out('-compose', 'over')
+	        .out('-composite')
+	        .out('-extent', `${width}x${height}`)
+	        .write(outputFile, (error) => {
+	          if (error) {
+	            return reject(error)
+	          }
+
+	          console.log(`> Image converted: ${inputFile}`)
+	          resolve()
+	        })
+
+	    })
+	}
+
+	async function createAllSentenceImages(content) {
+	    for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+	    	await createSetenceImage(sentenceIndex, content.sentences[sentenceIndex].text)
+	    }
+	}
+
+	async function createSetenceImage(sentenceIndex, sentenceText) {
+		return new Promise((resolve, reject) => {
+			const outputFile = `./content/${sentenceIndex}-sentence.png`
+
+			const templateSettings = {
+	        0: {
+	          size: '1920x400',
+	          gravity: 'center'
+	        },
+	        1: {
+	          size: '1920x1080',
+	          gravity: 'center'
+	        },
+	        2: {
+	          size: '800x1080',
+	          gravity: 'west'
+	        },
+	        3: {
+	          size: '1920x400',
+	          gravity: 'center'
+	        },
+	        4: {
+	          size: '1920x1080',
+	          gravity: 'center'
+	        },
+	        5: {
+	          size: '800x1080',
+	          gravity: 'west'
+	        },
+	        6: {
+	          size: '1920x400',
+	          gravity: 'center'
+	        }
+
+	      }
+
+	      gm()
+	        .out('-size', templateSettings[sentenceIndex].size)
+	        .out('-gravity', templateSettings[sentenceIndex].gravity)
+	        .out('-background', 'transparent')
+	        .out('-fill', 'white')
+	        .out('-kerning', '-1')
+	        .out(`caption:${sentenceText}`)
+	        .write(outputFile, (error) => {
+	          if (error) {
+	            return reject(error)
+	          }
+
+	          console.log(`> Sentence created: ${outputFile}`)
+	          resolve()
+	        })
+		})
+	}
+
+	async function createYouTubeThumbnail() {
+		return new Promise((resolve, reject) => {
+			gm()
+				.in('./content/0-converted.png')
+				.write('./content/youtube-thumbnail.jpg', (erro) => {
+					if(erro) {
+						return reject(erro)
+					}
+
+					console.log('> Creating YouTube Thumbnail')
+					resolve()
+				})
 		})
 	}
 }
